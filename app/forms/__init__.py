55e7e44602f9551b0ea8155327d4dd6c80bbb72d
JB Kam <jb3.kam@gmail.com>
2022-09-27 22:05:07 -0700

    imported forms in index file

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,2 +1,5 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
+from .buying_form import BuyingForm
+from .new_apparel_form import ApparelForm
+from .image_form import ImageForm
