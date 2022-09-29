b9e0b79bccdd8ac20d14337ddca296c45d5b1448
JB Kam <jb3.kam@gmail.com>
2022-09-28 23:59:34 -0700

    apparel post route doesnt have any loading errors

--- app/api/apparel_routes.py
+++ app/api/apparel_routes.py
@@ -14,31 +14,30 @@ def get_all_apparel():
 
 
 
-# @apparel_routes.route('/', methods=['POST'])
-# @login_required
-# def add_apparel():
-#     form = ApparelForm()
-#     form['csrf_token'].data = request.cookies['csrf_token']
+@apparel_routes.route("/", methods=["POST"])
+@login_required
+def add_apparel():
+    form = ApparelForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
 
-#     if form.validate_on_submit():
-#        new_apparel = Apparel(
-#             name = item["name"],
-#             description = item["description"],
-#             colorway = item["colorway"],
-#             release_date = item["release_date"],
-#             brand = item["brand"],
-#             style = item["style"],
-#             brand_type = item["brand_type"],
-#             condition = item["condition"],
-#             retail_price = item["retail_price"],
-#             price_sold = item["price_sold"],
-#             quantity_sold = item["quantity_sold"],
-#             size = item["size"]
-#         )
+    if form.validate_on_submit():
+    
+        new_apparel = Apparel(
+            name = item["name"],
+            description = item["description"],
+            colorway = item["colorway"],
+            release_date = item["release_date"],
+            brand = item["brand"],
+            style = item["style"],
+            brand_type = item["brand_type"],
+            condition = item["condition"],
+            retail_price = item["retail_price"],
+            price_sold = item["price_sold"],
+            quantity_sold = item["quantity_sold"],
+            size = item["size"]
+        )
+        db.session.add(new_apparel)
+        db.session.commit()
+        return 
 
-#         db.session.add(new_apparel)
-#         db.session.commit()
-        
-#         return {"Apparel": [items.to_dict() for items in apparel] }
-
-#     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
