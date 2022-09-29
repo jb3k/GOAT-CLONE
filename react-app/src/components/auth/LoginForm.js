b65c6241764b8b629c9c751ac9194e40768f2606
JB Kam <jb3.kam@gmail.com>
2022-09-29 11:09:12 -0700

    finished sign up and login forms

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Redirect } from 'react-router-dom';
+import { NavLink, useHistory, Redirect } from 'react-router-dom';
 import { login } from '../../store/session';
 import './LoginForm.css'
 
@@ -34,36 +34,70 @@ const LoginForm = () => {
   return (
     <>
       <div className='signup-navbar'>
-        StockY
+        <NavLink to='/' exact={true} activeClassName='active' style={{ color: 'rgb(7, 96, 7)', textDecoration: 'none' }}>
+          StockY
+        </NavLink>
       </div>
-      <form onSubmit={onLogin}>
-        <div>
-          {errors.map((error, ind) => (
-            <div key={ind}>{error}</div>
-          ))}
+      <div className='signup-whole-page'>
+        <div className='login-form-container'>
+          <div className='login-form-top-buttons-container'>
+            <div className='signup-form-top-lines'>
+              <div className='login-form-left-lines'>
+                <NavLink to='/sign-up' exact={true} activeClassName='active'>
+                  <button className='login-form-top-signup-button'>Sign Up </button>
+                </NavLink>
+              </div>
+              <p> </p>
+              <div className='login-form-right-lines'>
+                <NavLink to='/login' exact={true} activeClassName='active'>
+                  <button className='login-form-top-login-button'> Login </button>
+                </NavLink>
+              </div>
+            </div>
+          </div>
+
+          <div className='signup-form-body'>
+            <form onSubmit={onLogin}>
+              <div>
+                {errors.map((error, ind) => (
+                  <div key={ind}>{error}</div>
+                ))}
+              </div>
+              <div className='signup-form-body-container'>
+                <input
+                  className='signup-form-body-boxes'
+                  name='email'
+                  type='text'
+                  placeholder='   Email *'
+                  value={email}
+                  onChange={updateEmail}
+                  required={true}
+                />
+              </div>
+              <div className='signup-form-body-container'>
+                <input
+                  className='signup-form-body-boxes'
+                  name='password'
+                  type='password'
+                  placeholder='   Password *'
+                  value={password}
+                  onChange={updatePassword}
+                  required={true}
+                />
+              </div>
+              <div className='signup-form-body-container'>
+                <button className='signup-form-signup-button' type='submit'>Login</button>
+              </div>
+              <div className='signup-form-body-container'>
+                <button className='signup-form-signup-button' onClick={(e) => {
+                  setEmail('demouser@gmail.com');
+                  setPassword('password')
+                }}>Log In as Demo User</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            name='email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={updateEmail}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            name='password'
-            type='password'
-            placeholder='Password'
-            value={password}
-            onChange={updatePassword}
-          />
-          <button type='submit'>Login</button>
-        </div>
-      </form>
+      </div>
     </>
   );
 };
