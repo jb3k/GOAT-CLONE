7c88416a3f31489c97e6ae40158d57438cb5f3a4
JB Kam <jb3.kam@gmail.com>
2022-10-06 16:51:59 -0700

    updated the zipcode error

--- react-app/src/components/editUserPurchases/index.js
+++ react-app/src/components/editUserPurchases/index.js
@@ -30,12 +30,8 @@ const EditUserPurchase = ({ purchaseId, userAddy, userCity, userZip, userState, 
         if (nums.includes(city) || specialChar.includes(city) || city.length <= 1 || city.length > 30) errors.push('Invalid City')
         if (nums.includes(state) || specialChar.includes(state) || state.length !== 2) errors.push('Invalid Abbreviated State name')
         // if (country !== 'USA' || country !== 'usa') errors.push('Shipping is only USA')
-        if (zipcode.length !== 5) errors.push('Invalid Zipcode')
-
-        // if ((validationCharNums(city)) || city.length <= 1) errors.push('Invalid City')
-        // if ((validationCharNums(state)) || state.length <= 1) errors.push('Invalid State')
-        // if (country !== 'USA') errors.push('Shipping is only USA')
-        // if ((validationCharNums(zipcode))|| zipcode.length !== 5) errors.push('Invalid Zipcode')
+        if (zipcode.toString().length !== 5) errors.push('Invalid Zipcode')
+        // console.log(zipcode, zipcode.toString().length, (12345).length)
 
         setErrors(errors)
     }, [address, city, state, country, zipcode])
@@ -111,7 +107,7 @@ const EditUserPurchase = ({ purchaseId, userAddy, userCity, userZip, userState, 
                     </div>
                     <div>
                         <select
-                            style={{ width: '141px' }}
+                            style={{ width: '142px' }}
                         // type='text'
                         // name='country'
                         // onChange={e => setCountry(e.target.value)}
